6a92aead2faf0096c608a7e3fb8e8100cee6351f
ines derri <ines.derri@edu.univ-eiffel.fr>
2025-03-25 12:18:39 +0100

    Update admin_login.php mise en page pour css futur

--- views/admin_login.php
+++ views/admin_login.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Connexion Administrateur</title>
+    <link rel="stylesheet" href="assets/style/admin.css">
 </head>
 <body>
-    <h1>Connexion Administrateur</h1>
+    
     <form method="post" action="admin.php">
+        <h1>Connexion Administrateur</h1>
         <label>Email :</label><br>
         <input type="email" name="email" required><br>
         <label>Mot de passe :</label><br>
         <input type="password" name="password" required><br><br>
         <input type="submit" name="admin_login" value="Se connecter">
-    </form>
-    <?php if(isset($error)) { echo "<p style='color:red;'>{$error}</p>"; } ?>
+         <?php if(isset($error)) { echo "<p style='color:red;'>{$error}</p>"; } ?>
     <p><a href="reserv.php">Retour au site</a></p>
+    </form>
+   
 </body>
 </html>
